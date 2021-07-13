c4f9b1f70e094951afc46bb3327074a661df8445
gtretow <guilherme.tretow@gmail.com>
2021-07-13 00:44:39 -0300

    orkut started

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,45 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { AlurakutStyles } from "../src/lib/alurakutCommons";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  /* gera uma variavel que pode ser usada por todo site */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    
+  }
+  body {
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+  #__next {
+    /* setando altura minima para o app */
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+   ${AlurakutStyles} 
+`;
+
+const theme = {
+  colors: {
+    primary: "red",
+  },
+};
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-
-export default MyApp
